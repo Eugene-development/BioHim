711688c85a991c7cad9c6139a30989367e16953a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-21 15:09:11 +0300

    git init

---
+++ app/src/routes/+layout.js
@@ -0,0 +1,9 @@
+import { phone, email, menu } from "./layout.json"
+
+export const load = () => {
+    return {
+        phone,
+        email,
+        menu
+    };
+}
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
     import "../app.css";
+    import {Header_v1} from "symax/ui/broker"
+    export let data
+    console.log(data)
 </script>
 
-<p>123</p>
+
+
+<Header_v1 {...data}/>
 <main>
     <slot />
 </main>

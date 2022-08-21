ee1817a7f7bc83fc2981fc30902679b465dc0229
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-21 20:24:05 +0300

    git init

--- app/src/routes/+layout.js
+++ app/src/routes/+layout.js
@@ -1,5 +1,6 @@
 import { phone, email, menu } from "../data/header_v1.json"
 import { logo, menu as mainMenu } from "../data/header_v2.json"
+import { yearStart, text, phoneService, menu as footerMenu} from "../data/footer_v1.json"
 
 const content_Header_v1 = {
     phone,
@@ -10,10 +11,17 @@ const content_Header_v2 = {
     logo,
     menu: mainMenu
 }
+const content_Footer_v1 = {
+    yearStart,
+    text,
+    phoneService,
+    menu: footerMenu
+}
 
 export const load = () => {
     return {
         content_Header_v1,
-        content_Header_v2
+        content_Header_v2,
+        content_Footer_v1
     };
 }
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script>
     import "../app.css";
-    import {Header_v1, Header_v2} from "symax/ui/broker"
+    import {Header_v1, Header_v2, Footer_v1} from "symax/ui/broker"
     export let data
 </script>
 
-
-
 <Header_v1 {...data.content_Header_v1}/>
 <Header_v2 {...data.content_Header_v2}/>
-<main>
-    <slot />
-</main>
+    <main>
+        <slot />
+    </main>
+<Footer_v1 {...data.content_Footer_v1}/>

5d2e3dc3fc3794432bd36c05ebacd28d8c7ceb93
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-23 20:48:46 +0300

    git init

--- app/src/routes/+page.js
+++ app/src/routes/+page.js
@@ -1,10 +1,12 @@
 import {content_Main_v1} from "../data/main_v1.js"
 import {content_Marketing_v1} from "../data/marketing_v1.js"
+import {content_Marketing_v5} from "../data/marketing_v5.js"
 
 
 export const load = () => {
     return {
         content_Main_v1,
         content_Marketing_v1,
+        content_Marketing_v5,
     };
 }
---
+++ app/src/routes/information/application/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {Marketing_v5} from "symax/ui/broker"
+    export let data
+</script>
+
+<Marketing_v5 {...data.content_Marketing_v5}/>

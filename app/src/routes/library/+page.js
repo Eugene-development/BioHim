4234597b5a6ec2ad3c10ca169f61e4faf4f23dec
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-24 20:23:45 +0300

    git init

---
+++ app/src/routes/library/+page.js
@@ -0,0 +1,7 @@
+import {content_Article_v1} from "./article_v1.js";
+
+export const load = () => {
+    return {
+        content_Article_v1,
+    };
+}
---
+++ app/src/routes/library/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {Article_v1} from "symax/ui/broker"
+    export let data
+</script>
+
+<Article_v1 {...data.content_Article_v1}/>

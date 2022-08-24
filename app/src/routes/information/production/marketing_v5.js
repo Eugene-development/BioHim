9cf33266eba8ab7b9ff342b7dbfe77abd3dbf5f2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-24 12:40:16 +0300

    git init

--- app/src/routes/information/production/marketing_v5.js
+++ app/src/routes/information/production/marketing_v5.js
@@ -3,7 +3,7 @@ const text =
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus eligendi enim et in laborum, magni natus nesciunt officiis omnis quibusdam rem velit vitae voluptatibus.';
 const textButton = ''
 const img =
-    'https://images.unsplash.com/photo-1507207611509-ec012433ff52?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80';
+    'https://storage.yandexcloud.net/brand-logo/biohim/foto/99.jpg';
 
 export const content_Marketing_v5 = {
     title,

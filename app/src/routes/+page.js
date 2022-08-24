11656f57b22566ab5142697e04d28aa5c7fcdb19
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-24 12:48:53 +0300

    git init

--- app/src/routes/+page.js
+++ app/src/routes/+page.js
@@ -1,7 +1,6 @@
 import {content_Main_v1} from "../data/main_v1.js"
 import {content_Marketing_v1} from "../data/marketing_v1.js"
 
-
 export const load = () => {
     return {
         content_Main_v1,

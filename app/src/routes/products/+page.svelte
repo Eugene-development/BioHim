4c9c683b9f01f5413cdb47a77f9d76e5a9b2dd04
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-24 20:44:19 +0300

    git init

---
+++ app/src/routes/products/+page.svelte
@@ -0,0 +1,41 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="bg-gray-50">
+    <div class="max-w-7xl mx-auto py-12 px-4 divide-y divide-gray-200 sm:px-6 lg:py-16 lg:px-8">
+        <h2 class="text-3xl tracking-tight font-bold text-gray-900">Предлагаемая продукция</h2>
+        <div class="mt-8">
+            <dl class="divide-y divide-gray-200">
+                <div class="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                    <dt class="text-base font-medium text-gray-900 md:col-span-5">ТЕХНИЧЕСКАЯ ПЕРЕКИСЬ ВОДОРОДА</dt>
+                    <dd class="mt-2 md:mt-0 md:col-span-7">
+                        <p class="text-base text-gray-500">Марка А выпускается согласно ГОСТ 177-88 (35-40%)</p>
+                        <p class="text-base text-gray-500">Марка Б выпускается согласно ГОСТ 177-88 (30-40%)</p>
+                        <p class="text-base text-gray-500">Марка Б выпускается согласно ТУ 2123-002-25665344-2008 (50-52%)</p>
+                        <p class="text-base text-gray-500">Марка В выпускается согласно ТУ 2123-002-25665344-2008 (58-60%)</p>
+                    </dd>
+                </div>
+                <div class="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                    <dt class="text-base font-medium text-gray-900 md:col-span-5">МЕДИЦИНСКАЯ ПЕРЕКИСЬ ВОДОРОДА</dt>
+                    <dd class="mt-2 md:mt-0 md:col-span-7">
+                        <p class="text-base text-gray-500">Перекись водорода медицинская выпускается согласно ГОСТ 177-88</p>
+                    </dd>
+                </div>
+                <div class="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                    <dt class="text-base font-medium text-gray-900 md:col-span-5">АСЕПТИЧЕСКАЯ ПЕРЕКИСЬ ВОДОРОДА</dt>
+                    <dd class="mt-2 md:mt-0 md:col-span-7">
+                        <p class="text-base text-gray-500">Марка Пуроксид асептик С выпускается согласно ТУ 2123-004-25665344-2009 (35-36%)</p>
+                        <p class="text-base text-gray-500">Марка Пуроксид асептик выпускается согласно ТУ 2123-004-25665344-2009 (35-36%)</p>
+                    </dd>
+                </div>
+                <div class="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                    <dt class="text-base font-medium text-gray-900 md:col-span-5">ПЕРЕКИСЬ ВОДОРОДА ОСОБОЙ ЧИСТОТЫ</dt>
+                    <dd class="mt-2 md:mt-0 md:col-span-7">
+                        <p class="text-base text-gray-500">Марка ОСЧ 8-4 (30-35%)</p>
+                        <p class="text-base text-gray-500">Марка ОСЧ 33-4 (29-31%)</p>
+                    </dd>
+                </div>
+
+                <!-- More questions... -->
+            </dl>
+        </div>
+    </div>
+</div>

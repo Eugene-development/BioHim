5db8077c0e27d16fabd4c5915f1bb3fa51f55cbd
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-28 22:32:46 +0300

    git init

--- app/src/data/main_v1.js
+++ app/src/data/main_v1.js
@@ -7,7 +7,7 @@ const link = {
 const H1 = `
 <h1 class="mt-4 tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 text-4xl lg:text-4xl xl:text-5xl antialiased">
 <span class="md:block">Перекись водорода</span>
-<span class="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-cyan-400 sm:pb-5">H2O2</span>
+<span class="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-cyan-400 sm:pb-5">Н<span class="text-lg">2</span>O<span class="text-lg">2</span></span>
 </h1>
 `;
 

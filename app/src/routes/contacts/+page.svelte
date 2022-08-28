ace16fe7552a04f2462b3666544542c855b93f83
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-28 22:49:27 +0300

    git init

--- app/src/routes/contacts/+page.svelte
+++ app/src/routes/contacts/+page.svelte
@@ -53,7 +53,7 @@
             <div>
                 <h2 class="text-2xl font-extrabold text-gray-900 sm:text-3xl">Отдел по работе с клиентами</h2>
                 <div class="mt-3">
-                    <p class="text-lg text-gray-500">Связь с ведущим специалистом</p>
+                    <p class="text-lg text-gray-500">Связь с ведущим специалистом:</p>
                 </div>
                 <div class="mt-9">
                     <div class="flex">
@@ -65,7 +65,7 @@
                         </div>
                         <div class="ml-3 text-base text-gray-500">
                             <p>+7 (499) 777-77-77</p>
-                            <p class="mt-1">Пн-Вс с 10-00 до 19-00</p>
+<!--                            <p class="mt-1">Пн-Вс с 10-00 до 19-00</p>-->
                         </div>
                     </div>
                     <div class="mt-6 flex">
@@ -79,61 +79,22 @@
                             <p>info@biohim.org</p>
                         </div>
                     </div>
-<!--                    <div class="mt-6 flex">-->
-<!--                        <div class="flex-shrink-0">-->
-<!--                            &lt;!&ndash; Heroicon name: outline/mail &ndash;&gt;-->
-<!--                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">-->
-<!--                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />-->
-<!--                            </svg>                        </div>-->
-<!--                        <div class="ml-3 text-base text-gray-500">-->
-<!--                            <a href="https://t.me/EvgeniyChe">Телеграм: @EvgeniyChe</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
+                    <div class="mt-6 flex">
+                        <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/mail -->
+                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>                        </div>
+                        <div class="ml-3 text-base text-gray-500">
+                            <a href="https://t.me/biohim">Телеграм: @biohim</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <!--            <div class="mt-12 sm:mt-16 md:mt-0">-->
-            <!--                <h2 class="text-2xl font-extrabold text-gray-900 sm:text-3xl">Бригадир мастеров</h2>-->
-            <!--                <div class="mt-3">-->
-            <!--                    <p class="text-lg text-gray-500">Связь по вопросам выполнения текущих проектов, организации работ и рекламаций.</p>-->
-            <!--                </div>-->
-            <!--                <div class="mt-9">-->
-            <!--                    <div class="flex">-->
-            <!--                        <div class="flex-shrink-0">-->
-            <!--                            &lt;!&ndash; Heroicon name: outline/phone &ndash;&gt;-->
-            <!--                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
-            <!--                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />-->
-            <!--                            </svg>-->
-            <!--                        </div>-->
-            <!--                        <div class="ml-3 text-base text-gray-500">-->
-            <!--                            <p>+7 (930) 276-56-50</p>-->
-            <!--                            <p class="mt-1">Пн-Вс с 8-00 до 21-00</p>-->
-            <!--                        </div>-->
-            <!--                    </div>-->
-            <!--                    <div class="mt-6 flex">-->
-            <!--                        <div class="flex-shrink-0">-->
-            <!--                            &lt;!&ndash; Heroicon name: outline/mail &ndash;&gt;-->
-            <!--                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
-            <!--                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />-->
-            <!--                            </svg>-->
-            <!--                        </div>-->
-            <!--                        <div class="ml-3 text-base text-gray-500">-->
-            <!--                            <p>buildnovostroi@gmail.com</p>-->
-            <!--                        </div>-->
-            <!--                    </div>-->
-            <!--                    <div class="mt-6 flex">-->
-            <!--                        <div class="flex-shrink-0">-->
-            <!--                            &lt;!&ndash; Heroicon name: outline/mail &ndash;&gt;-->
-            <!--                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">-->
-            <!--                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />-->
-            <!--                            </svg>                        </div>-->
-            <!--                        <div class="ml-3 text-base text-gray-500">-->
-            <!--                            <a href="https://t.me/NovoStrouy">Телеграм: @NovoStrouy</a>-->
-            <!--                        </div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--            </div>-->
+            <div class="mt-16 sm:mt-24 md:mt-0">
+                <img class="mx-auto h-full" src="https://storage.yandexcloud.net/brand-logo/biohim/logo/BioHim_free-file.png" alt="logo">
+            </div>
         </div>
-        <!--        <img class="mx-auto h-96" src="/logo.png" alt="logo">-->
 
 
 
